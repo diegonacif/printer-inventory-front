ad3f623cc03569fe2ba2270efc522fc119578622
jarbson costa <jarbsonfc@gmail.com>
2021-07-11 17:42:57 -0300

    criando context de Auth

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { shade } from 'polished'
+
+
+
+export const Container = styled.button`
+
+
+  background: #4CAF50;
+    height: 50px;
+    border-radius: 5px;/*  borda arredondada*/
+    border: 0;
+    padding: 0 16px;
+    color: #fff;
+    width: 100%;
+    font-weight: 500;
+    margin-top: 35px;
+    transition: background-color 0, 2s;
+    outline: none !important;
+    box-shadow: 4px 9px 17px #888888;
+
+    &:hover {
+        background: ${shade(0.3, '#4CAF50')}; /* escurecer 20% a cor do botão*/
+    }
+
+
+
+
+`
--- src/components/inputs/styles.ts
+++ src/components/inputs/styles.ts
@@ -3,8 +3,39 @@ import styled from "styled-components";
 
 
 
+
 export const Container = styled.div`
 
+ background:  #fff;
+  border-radius: 5px;/*   borda aredondada*/
+    padding: 10px;
+    width: 100%;
+    border: 1px solid #bfc3c5;
 
 
-`
+    color: #666360  ;
+
+    display: flex;
+    align-items: center;
+
+
+
+
+    input {
+        flex: 1;
+        background: transparent;
+        border: 0;
+        color: #262726;
+        padding-top:10px;
+
+        &::placeholder {
+            color: #bfc3c5;
+
+        }
+    }
+
+`;
+
+
+
+
